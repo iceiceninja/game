6cbf907ce237790b6bcfdb42d849d5b9efb42120
iceiceninja <joshdbonham@gmail.com>
2022-08-22 22:22:32 -0500

    FIxed Css and Js not loading

---
+++ app.js
@@ -0,0 +1,17 @@
+const PORT = 3000;
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname));
+// app.use(express.static('public'))
+app.use('/styles', express.static(__dirname + 'public/styles'))
+app.use(express.static('scripts'))
+app.use('scripts', express.static(__dirname + 'scripts'))
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res)=>{
+    res.render('index')
+})
+
+app.listen(PORT,()=>console.log("listening on port " + (PORT)))
--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,3 @@
-const PORT = 3000;
-const express = require('express')
-const app = express()
-
-
-app.use(express.static('Public'))
-app.use('/styles', express.static(__dirname + 'Public/styles'))
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-app.get('', (req, res)=>{
-    res.render('index')
-})
-
-app.listen(PORT,()=>console.log("listening on port " + (PORT)))
-
 
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
